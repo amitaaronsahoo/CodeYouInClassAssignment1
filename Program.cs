cf90b38d0d99d46d48a15f2cf1b06c8153cbe715
amitaaronsahoo <amitaaronsahoo@gmail.com>
2026-03-18 11:31:02 -0400

    Fixed missing semicolon

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         string a; 
-        Console.WriteLine($"Type something")
+        Console.WriteLine($"Type something");
 
         a = Console.ReadLine(); 
         
